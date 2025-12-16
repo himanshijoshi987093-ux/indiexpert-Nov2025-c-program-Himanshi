403607a833ff11d42a84a3d38b4669d26ceff3eb
himanshijoshi987093-ux <himanshijoshi987093@gmail.com>
2025-12-16 14:21:33 +0530

    This is my first github practice class

---
+++ secondgithub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ 
+    printf("\n**** Hello Himanshi Joshi****\n");
+    printf(" \n *** Today I have learnt three things that is fllowing ***\n");
+    printf( "** first is clone **\n");
+    printf(" ** second is push**\n");
+    printf(" *third is pull*\n");
+
+
+    return 0;
+}
