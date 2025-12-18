0bae6932cd69aab56ad72b25971e2e0b2b1284f1
himanshijoshi987093-ux <himanshijoshi987093@gmail.com>
2025-12-18 23:15:55 +0530

    relational

---
+++ newfolderfor18/practiceofoperator.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+int firstnum;
+int secondnum;
+printf(" \n******Relational operator******\n");
+printf(" plese enter the any first number:");
+scanf(" \n%d",&firstnum);
+printf(" please enter the any second number:");
+scanf("\n %d",&secondnum);
+
+
+printf("\n*****Equal operator:%d*****\n",(firstnum==secondnum));
+printf(" \n**** Greater than operator****:%d\n",(firstnum>secondnum));
+printf(" \n***Less than operator***:%d\n",(firstnum<secondnum));
+printf("\n **Greater than equal operator**:%d\n",(firstnum>=secondnum));
+printf("\n *Less than equal operator*:%d\n",(firstnum<=secondnum));
+printf("\n 0Inequality operator0:%d\n",(firstnum!=secondnum));
+
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
